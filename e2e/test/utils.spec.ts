6368bb31e8d93088695c11b27720892b7c0e5c34
Ondrej Urik <ondrej.urik@live.com>
2018-06-12 13:16:49 +0100

    Updated sample tests

--- e2e/test/utils.spec.ts
+++ e2e/test/utils.spec.ts
@@ -4,7 +4,7 @@ import { using } from './../../src/index';
 
 describe('utils', () => {
 
-    describe('isEmpty using', () => {
+    describe('using sample tests', () => {
         using(null).
         using(undefined).
             it('should return true when value is null or undefined', value => {
@@ -13,7 +13,7 @@ describe('utils', () => {
             });
     });
 
-    describe('isEmpty cases', () => {
+    describe('cases sample tests', () => {
         using.cases(null, undefined).
             it('should return true when value is null or undefined', value => {
                 const result = Utils.isEmpty(value);
@@ -21,7 +21,7 @@ describe('utils', () => {
             });
     });
 
-    describe('smoke tests', () => {
+    describe('using smoke tests', () => {
         using(null).
         using(undefined).
             it('should check if argument is null or undefined', arg => {
@@ -39,12 +39,13 @@ describe('utils', () => {
         // pending
         using('blue').
             it('should show pending placeholder');
-        // skip
+        // it.skip
         using(null).
         using(undefined).
             it.skip('should skip when it.skip used', sp => {
                 expect(sp).to.not.be.a('number');
             });
+        // this.skip
         using('one').
         using(false).
             it('should skip when this.skip used', function (sp) {
@@ -52,12 +53,13 @@ describe('utils', () => {
                 const result = sp !== true;
                 expect(result).to.equal(false);
             });
+        // xit
         using(1).
-        using('a')
-            .xit('should skip when using.xit used', p => {
+        using('a').
+            xit('should skip when using.xit used', p => {
                 expect(p).to.equal(false);
             });
-        // retries
+        // .retries
         let retryA = 1;
         using(2.53).
         using(3.14).
@@ -67,6 +69,7 @@ describe('utils', () => {
                 expect(retryA).to.be.greaterThan(5);
             })
             .retries(4);
+        // this.retries
         let retryB = 1;
         using(1.53).
         using(7.14).
@@ -75,7 +78,7 @@ describe('utils', () => {
                 retryB += num;
                 expect(retryB).to.be.greaterThan(3);
             });
-        // slow
+        // .slow
         using(20).
         using(50).
             it('should define slow with it.slow', (tm, done) => {
@@ -84,6 +87,7 @@ describe('utils', () => {
                     done();
                 }, tm);
             }).slow(10);
+        // this.slow
         using(15).
         using(25).
             it('should define slow with this.slow', function(tm, done) {
@@ -93,10 +97,20 @@ describe('utils', () => {
                     done();
                 }, tm);
             });
-        // timeout
+        // .timeout
         using(80).
         using(40).
             it('should expose timeout in async function', (tm, done) => {
+                setTimeout(() => {
+                    expect(done).to.be.a('function');
+                    done();
+                }, tm);
+            }).timeout(160);
+        // this.timeout
+        using(60).
+        using(30).
+            it('should define timeout with this.timeout', function(tm, done) {
+                this.timeout(120);
                 setTimeout(() => {
                     expect(done).to.be.a('function');
                     done();
@@ -107,8 +121,104 @@ describe('utils', () => {
         using(true, /w/, Infinity).
         using(false, /a/, -Infinity).
         using(null, /z/, -NaN).
-            it('anca', (arg1, arg2, arg3) => {
-                expect(arg1).to.not.equal(null);
+            it('should resolve last argument as done function', (arg1, arg2, arg3, done) => {
+                expect('' + arg1 + arg2 + arg3).to.be.a('string');
+                done();
+            });
+    });
+
+    describe('cases smoke tests', () => {
+        using.cases(null, undefined).
+            it('should check if argument is null or undefined', arg => {
+                expect(arg === null || arg === undefined).to.equal(true);
+            });
+        // done
+        using.cases(1, 2, 3).
+            it('should run done as the last argument', (arg, done) => {
+                arg = Math.random();
+                expect(done).to.be.a('function');
+                done();
+            });
+        // pending
+        using.cases('blue').
+            it('should show pending placeholder');
+        // it.skip
+        using.cases(null, undefined).
+            it.skip('should skip when it.skip used', sp => {
+                expect(sp).to.not.be.a('number');
+            });
+        // this.skip
+        using.cases('one', false).
+            it('should skip when this.skip used', function (sp) {
+                this.skip();
+                const result = sp !== true;
+                expect(result).to.equal(false);
+            });
+        // xit
+        using.cases(1, 'a').
+            xit('should skip when using.xit used', p => {
+                expect(p).to.equal(false);
+            });
+        // .retries
+        let retryA = 1;
+        using.cases(2.53, 3.14, 7.1).
+            it('should retry with it.retries', num => {
+                retryA += num;
+                expect(retryA).to.be.greaterThan(5);
+            })
+            .retries(4);
+        // this.retries
+        let retryB = 1;
+        using.cases(1.53, 7.14).
+            it('should retry with this.retries', function(num) {
+                this.retries(4);
+                retryB += num;
+                expect(retryB).to.be.greaterThan(3);
+            });
+        // .slow
+        using.cases(20, 50).
+            it('should define slow with it.slow', (tm, done) => {
+                setTimeout(() => {
+                    expect(done).to.be.a('function');
+                    done();
+                }, tm);
+            }).slow(10);
+        // this.slow
+        using.cases(15, 25).
+            it('should define slow with this.slow', function(tm, done) {
+                this.slow(10);
+                setTimeout(() => {
+                    expect(done).to.be.a('function');
+                    done();
+                }, tm);
+            });
+        // .timeout
+        using.cases(80, 40).
+            it('should expose timeout in async function', (tm, done) => {
+                setTimeout(() => {
+                    expect(done).to.be.a('function');
+                    done();
+                }, tm);
+            }).timeout(160);
+        // this.timeout
+        using.cases(60, 30).
+            it('should define timeout with this.timeout', function(tm, done) {
+                this.timeout(120);
+                setTimeout(() => {
+                    expect(done).to.be.a('function');
+                    done();
+                }, tm);
+            }).timeout(160);
+        // multiple arguments
+        using.cases(
+            [10, 20, 30],
+            [true, /w/, Infinity],
+            [false, /a/, -Infinity],
+            [null, /z/, -NaN]
+        ).
+            it('should resolve last argument as done function', (arg1, arg2, arg3, done) => {
+                expect('' + arg1 + arg2 + arg3).to.be.a('string');
+                done();
             });
     });
 

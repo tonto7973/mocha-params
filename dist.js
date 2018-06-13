6a5146fdb417086bce5288a2093a0dd936cc55f5
Ondrej Urik <ondrej.urik@live.com>
2018-06-13 08:58:40 +0100

    Updated build

---
+++ dist.js
@@ -0,0 +1,65 @@
+var fs = require('fs');
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const targetDir = './dist';
+const copy = (src, dest) => fs.writeFileSync(dest || (targetDir + '/' + src), fs.readFileSync(src)); //fs.copyFileSync(src, dest || targetDir);
+const read = src => fs.readFileSync(src).toString('utf8');
+const write = (dest, data) => fs.writeFileSync(targetDir + '/' + dest, Buffer.from(data, 'utf8'));
+const readJSON = src => JSON.parse(read(src));
+const writeJSON = (dest, data) => write(dest, JSON.stringify(data, null, 2));
+const tasks = {
+    tasks:[],
+    add: (name, fn) => {
+        tasks.tasks.push({
+            name: name,
+            run: fn
+        });
+    },
+    run: () => {
+        tasks.tasks.every(task => {
+            console.log('' + task.name);
+            try {
+                task.run();
+                return true;
+            } catch (ex) {
+                console.error(ex);
+                return false;
+            }
+        })
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+tasks.add('copy readme and license', () => {
+    copy('README.md');
+    copy('LICENSE');
+});
+
+tasks.add('setup package.json', () => {
+    var packageJSON = readJSON('package.json');
+
+    packageJSON.main = './index.js';
+    packageJSON.typings = './index.d.ts';
+    packageJSON.scripts = undefined;
+    packageJSON.nyc = undefined;
+    packageJSON.devDependencies = undefined;
+
+    writeJSON('package.json', packageJSON);
+});
+
+tasks.add('setup typings', () => {
+    var data = read('./src/index.d.ts');
+    var exportFound = false;
+    read('./src/index.ts').split(/([\r|\n]+)/g).forEach(line => {
+        exportFound = exportFound || /(^|\s)export[\s]+declare[\s]+/.test(line);
+        if (exportFound) {
+            data += line;
+        }
+    });
+    data += '\r\nexport default using;\r\n';
+    write('index.d.ts', data);
+});
+
+tasks.run();
